d9692e96bbb822e76f93397ae3aa031cca12f9ae
darryl1975 <darryl1975@hotmail.com>
2024-10-03 09:44:17 +0800

    completed day01 lecture

--- myapp/HelloWorld.java
+++ myapp/HelloWorld.java
@@ -3,6 +3,7 @@ package myapp;
 import java.io.Console;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class HelloWorld {
 
@@ -91,23 +92,45 @@ public class HelloWorld {
         // System.out.println("No argument passed");
         // }
 
-        List<String> names = new ArrayList<>();
+        // List<String> names = new ArrayList<>();
 
-        String input = "";
-        do {
-            input = console.readLine("Enter a name: ");
-            if (!input.equals("quit")) {
-                names.add(input);
-            }
+        // String input = "";
+        // do {
+        //     input = console.readLine("Enter a name: ");
+        //     if (!input.equals("quit")) {
+        //         names.add(input);
+        //     }
 
-        } while (!input.equals("quit"));
+        // } while (!input.equals("quit"));
 
-        // print out the names
-        for(String name: names) {
-            System.out.println(name);
+        // // print out the names
+        // for(String name: names) {
+        //     System.out.println(name);
+        // }
+
+        List<String> words = new ArrayList<>();
+
+        String keyboardInput = "";
+        keyboardInput = console.readLine("Enter a sentence: ");
+        keyboardInput = keyboardInput.replace(',', ' ');
+
+        Scanner scan = new Scanner(keyboardInput);
+        while(scan.hasNext()) {
+            words.add(scan.next());
         }
-        
+        scan.close();
 
+        for(int z=0; z < words.size(); z++) {
+            // System.out.printf("%d : %s \r\n", z+1, words.get(z));
+            System.out.println(z+1 + " : " + words.get(z));
+        }
+
+        Scanner scan2 = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String item = scan2.next();
+        String item2 = scan2.nextLine();
+        System.out.println(item);
+        System.out.println(item2);
     }
 
 }
